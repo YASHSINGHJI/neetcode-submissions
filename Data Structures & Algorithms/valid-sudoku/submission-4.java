0cca9c4ea99a2644f99e69ed4d9b3d53b89390f7
Abhinav Singh <abhinavsingh12347@gmail.com>
2026-09-22 15:40:44 +0530

    Add: valid-sudoku - submission-4

---
+++ Data Structures & Algorithms/valid-sudoku/submission-4.java
@@ -0,0 +1,59 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<Character> set1 = new HashSet<>();
+        HashSet<Character> set2 = new HashSet<>();
+        HashSet<Character> set3 = new HashSet<>();
+        HashSet<Character> set4 = new HashSet<>();
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') {
+                    continue;
+                }
+                if (set1.contains(board[i][j]))
+                    return false;
+                set1.add(board[i][j]);
+            }
+            set1.clear();
+        }
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[j][i] == '.') {
+                    continue;
+                }
+                if (set2.contains(board[j][i]))
+                    return false;
+                set2.add(board[j][i]);
+            }
+            set2.clear();
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = i * 3; k <= (i * 3) + 2; k++) {
+                    for (int l = j * 3; l <= (j * 3) + 2; l++) {
+                        if (board[k][l] == '.')
+                            continue;
+                        if (set3.contains(board[k][l]))
+                            return false;
+                        set3.add(board[k][l]);
+                    }
+                }
+                set3.clear();
+            }
+        }
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = i * 3; k <= (i * 3) + 2; k++) {
+                    for (int l = j * 3; l <= (j * 3) + 2; l++) {
+                        if (board[l][k] == '.')
+                            continue;
+                        if (set4.contains(board[l][k]))
+                            return false;
+                        set4.add(board[l][k]);
+                    }
+                }
+                set4.clear();
+            }
+        }
+        return true;
+    }
+}
